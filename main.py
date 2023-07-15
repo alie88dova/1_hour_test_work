9e13b285720f27f2328f1e09c67858e4d87a1188
Ivan <73252601+alie88dova@users.noreply.github.com>
2023-07-15 20:16:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram.utils import executor
 from create_bot import dp
 from handlers import client
 import asyncio
-from handlers.events import schulded, start_work
+from handlers.events import append_to_list, start_work
 
 
 async def on_startup(_):
@@ -11,7 +11,7 @@ async def on_startup(_):
 client.register_handlers_client(dp)
 
 loop = asyncio.get_event_loop()
-loop.create_task(schulded())
+loop.create_task(append_to_list())
 loop.create_task(start_work())
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
