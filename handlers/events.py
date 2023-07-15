3577e4c341c33d88f06db71c511694935f274aee
Ivan <73252601+alie88dova@users.noreply.github.com>
2023-07-15 20:02:18 +0300

    4

--- handlers/events.py
+++ handlers/events.py
@@ -1,11 +1,12 @@
 import asyncio
 import hashlib
 import sqlite3
-from aiogram.utils.exceptions import ChatNotFound
+from keyboards.client_kb import kb_blocks_end
 from create_bot import bot
-from aiogram import types, Dispatcher
 from handlers.google_sheets import get_sheet
 from to_work import list_Tasks, tel_id
+import threading
+
 
 class Task():
 
@@ -18,9 +19,9 @@ class Task():
         self.time = task["time"]
         self.time_to_do = task["time_to_do"]
         #c помощью индетификатора отправляет готовность задачи
-        self.idintificatore = hashlib.sha512(
+        self.idintificatore = str(hashlib.sha512(
             str(task["tel_id"])+str(task["test_text"])
-        )
+        ))
 
     def end_timer(self):
         if self.ended == False:
@@ -30,7 +31,7 @@ class Task():
         await bot.send_message(self.tel_id, f"Ваша зачдача {self.test_text}\n"
                                             f"время старта {self.date} {self.time}"
                                             f"окончание через {self.time_to_do}!",
-                               reply_markup=)
+                               reply_markup= await kb_blocks_end(self.idintificatore))
 
 
 async def schulded():
@@ -43,3 +44,9 @@ async def schulded():
 
 
 async def start_work():
+    while True:
+        for task in list_Tasks:
+            delay = task.time_to_do
+            timer = threading.Timer(delay, task.end_timer)
+            timer.start()
+        await asyncio.sleep(60)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram.utils import executor
 from create_bot import dp
 from handlers import client
 import asyncio
-from handlers.events import schulded
+from handlers.events import schulded, start_work
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
@@ -13,6 +13,6 @@ client.register_handlers_client(dp)
 
 loop = asyncio.get_event_loop()
 loop.create_task(schulded())
-loop.create_task()
+loop.create_task(start_work())
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
